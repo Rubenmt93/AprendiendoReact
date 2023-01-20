632afe8a1d0e0d0b2a492d5b420080f6905e042e
Ruben Montes <rubenmontes330@gmail.com>
2023-01-20 22:25:47 +0000

    Personalizacion del componente App  y uso del inline style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+const estilo2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,0.5)'
+}
 
+const estilo= ({bg = '#222'}) => ({ 
+  backgroundColor:bg,
+  color: '#fff',
+  padding:'10px 15px',
+  margin:'10px 15px'
+})
+
+function Li({children}) {
+  
+  return(
+    <li style={{...estilo2, ...estilo({bg: '#333'})}} className='clase-li'>{children}</li>
+  )
+}
 function App() {
+  const valor = 'feliz'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul style={estilo({bg: '#750'})} className='clase-css'>
+      <Li estado = 'feliz'> valor de li</Li> 
+    </ul>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const Li = ({ children, estado, casa, edad }) => {
-  console.log(casa, edad);
-  return (
-    <li>{children} {estado}</li>
-  )
-}
-const X = () => 
-<ul>
-  <Li 
-    estado = {'feliz'}
-    casa = {false}
-    edad= {24}
-  >
-      Chancho
-  </Li>
-  <Li  estado = {'triste'}>Chancho</Li>
-  <Li  estado = {'cansado'}>Chancho</Li>
+// const Li = ({ children, estado, casa, edad }) => {
+//   console.log(casa, edad);
+//   return (
+//     <li>{children} {estado}</li>
+//   )
+// }
+// const X = () => 
+// <ul>
+//   <Li 
+//     estado = {'feliz'}
+//     casa = {false}
+//     edad= {24}
+//   >
+//       Chancho
+//   </Li>
+//   <Li  estado = {'triste'}>Chancho</Li>
+//   <Li  estado = {'cansado'}>Chancho</Li>
   
-</ul>
+// </ul>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <X />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  // <X />
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
