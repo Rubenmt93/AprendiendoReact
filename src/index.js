3a8288cf902d7765fb9a1c39ad1a99f2fb90a2a5
Ruben Montes <rubenmontes330@gmail.com>
2023-01-20 22:03:38 +0000

    Crear componente de prueba

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const X = () => <p> Hola mundo </p>
-root.render(
-  <X />
-  
-);
 
+const Li = ({ children, estado, casa, edad }) => {
+  console.log(casa, edad);
+  return (
+    <li>{children} {estado}</li>
+  )
+}
+const X = () => 
+<ul>
+  <Li 
+    estado = {'feliz'}
+    casa = {false}
+    edad= {24}
+  >
+      Chancho
+  </Li>
+  <Li  estado = {'triste'}>Chancho</Li>
+  <Li  estado = {'cansado'}>Chancho</Li>
+  
+</ul>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <X />
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
