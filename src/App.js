7e30c1e976a78835799747d9a1b8c38434401bef
Ruben Montes <rubenmontes330@gmail.com>
2023-01-21 11:15:48 +0000

    renderizar conponentes de un array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './main.css';
 import './button'
 import Button from './button';
+
+const arr = ['chacho 1','chacho 2','chacho 3']
 const App =()  => {
-  const flag=true
+  const flag=false
   if( flag){
     return <p> mi variable dio true</p>
   }
   return (
     <div>
       <h1> Hola Mundo</h1>
+      {arr.map(el=> <p key={el}> {el}</p>)}
       <Button onClick= { () => {console.log('click');}}>
         Enviar
       </Button>
