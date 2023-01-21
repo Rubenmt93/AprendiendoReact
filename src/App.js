4465312766151cbd360436e4a047fd5e856ac094
Ruben Montes <rubenmontes330@gmail.com>
2023-01-21 10:23:07 +0000

    agregar stilos con clases de css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './main.css';
 const estilo2 = {
   boxShadow: '0 5px 3px rgba(0,0,0,0.5)'
 }
@@ -14,14 +14,14 @@ const estilo= ({bg = '#222'}) => ({
 function Li({children}) {
   
   return(
-    <li style={{...estilo2, ...estilo({bg: '#333'})}} className='clase-li'>{children}</li>
+    <li  className='clase-li'>{children}</li>
   )
 }
 function App() {
   const valor = 'feliz'
   return (
-    <ul style={estilo({bg: '#750'})} className='clase-css'>
-      <Li estado = 'feliz'> valor de li</Li> 
+    <ul className='clase-css'>
+      <Li estado = 'feliz'> {valor}</Li> 
     </ul>
   );
 }
